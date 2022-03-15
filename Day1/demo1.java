0fdd01921c1f1e3803c869d6ff31dc1673d530d2
TQhub <101624250+TQhub@users.noreply.github.com>
2022-03-15 13:18:28 +0530

    Update demo1.java

--- Day1/demo1.java
+++ Day1/demo1.java
@@ -6,7 +6,7 @@ public class demo1 {
 	public static void main(String[] args) {
 // TODO Auto-generated method stub
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Desktop\\Sel-pro\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Desktop\\Sel-pro\\chromedriver.exe");  //chrome driver path
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://google.com");
 											}
